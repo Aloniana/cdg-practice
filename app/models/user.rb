1cfccebe7224b50e2e0386dba48c3788908573ae
Aloniana <avkibalova@gmail.com>
2022-01-01 17:30:10 +0300

    Lab works 6 and 7 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, email: true, length: { maximum: 100 }
   validates :first_name, presence: true, length: { maximum: 100 }
